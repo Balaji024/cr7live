ff047eaaa3c3f1476e770ee2a602edefb329c54d
EC2 Default User <ec2-user@ip-172-31-24-118.us-east-2.compute.internal>
2021-08-03 12:00:13 +0000

    version 1.7

--- test.groovy
+++ test.groovy
@@ -1,14 +1,13 @@
 println("Hello world")
-'cd cr7live'.execute()
-//def gitLog = ['git', 'log', '--oneline'].execute().text
-//println(gitLog)
+//'cd cr7live'.execute()
+def gitLog = ['git', 'log', 'origin/master...1.6','--oneline'].execute()
+println(gitLog)
 
 //def gitLog() { 
  // ['git', 'log', "--pretty=format:* %h: %s", 'master..HEAD'].execute().text.trim() 
 //}
-
 String output = new StringWriter().with { out ->
-    Process proc = ['git', 'log', '--oneline'].execute()
+    Process proc = ['echo','hello'].execute()
     proc.consumeProcessOutput( out, System.err )
     proc.waitFor()
     out.toString()
